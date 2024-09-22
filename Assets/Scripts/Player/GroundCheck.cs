0f875c287f4c43d3164db688f832c48b3689defd
Deionara <legenddeionara2288@gmail.com>
2024-09-22 14:34:42 +0200

    grafon

--- Assets/Scripts/Player/GroundCheck.cs
+++ Assets/Scripts/Player/GroundCheck.cs
@@ -6,9 +6,10 @@ public class GroundCheck : MonoBehaviour
     public bool is_Ground = true;
     public float distance = 1.5f;
     public LayerMask layer;
-    private void Update()
+
+    public bool IsGrounded()
     {
-        is_Ground = Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, distance, layer);
+        return Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, distance, layer);
     }
 
     private void OnDrawGizmos()
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float moveSpeed = 550f;
+    public float moveFriction = 15f;
+
+    Rigidbody rb;
+    public GroundCheck check;
+    public Vector3 velocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        speed = moveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementHandle();
+    }
+
+    void MovementHandle()
+    {
+        Vector2 inputDir = new Vector2( Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (!check.IsGrounded())
+        {
+            velocity.y -= 9.8f * Time.deltaTime;
+        }
+
+        if (check.IsGrounded())
+        {
+            if (inputDir != Vector2.zero)
+            {
+                velocity.x = Mathf.Lerp(velocity.x, transform.forward.z * inputDir.x * speed, 5);
+                velocity.z = Mathf.Lerp(velocity.z, transform.forward.z * inputDir.y * speed, 5);
+            }
+            else 
+            {
+                velocity.x = Mathf.Lerp(velocity.x, 0, moveFriction * Time.deltaTime);
+                velocity.z = Mathf.Lerp(velocity.z, 0, moveFriction * Time.deltaTime);
+            }
+            rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
+        }
+    }
+}
